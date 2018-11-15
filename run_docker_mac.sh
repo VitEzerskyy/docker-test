dfab886f79648d0ef6f24bbb0e4ca8b1b51fffe2
Anton Lipovskih <anton.lipovskih@wikrgroup.com>
2018-11-15 15:48:03 +0200

    docker for mac --> docker-machine

--- run_docker_mac.sh
+++ run_docker_mac.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
+docker-machine create -d virtualbox --virtualbox-memory "2048" --virtualbox-cpu-count "4" default
+
+docker_run=$(docker-machine ls | grep default)
+echo $docker_run
+
+if [[ "$docker_run" == *"Saved"* ]] || [[ "$docker_run" == *"Stopped"* ]]
+then
+    docker-machine start default
+    docker-machine-nfs default
+    docker-machine env default
+    eval "$(docker-machine env default)"
+elif [[ "$docker_run" == *"Running"* ]]
+then
+    docker-machine-nfs default
+    docker-machine env default
+    eval "$(docker-machine env default)"
+fi
+
 UID_VAR="{{ UID_VALUE }}"
 UID_VALUE=$(id -u)
 if [ $(id -u) -eq 0 ]; then
